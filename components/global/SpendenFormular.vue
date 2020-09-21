88912075514d31d2880a72e1606aa381683e21da
Grzegorz Leoniec <greg@app-interactive.de>
2020-09-21 21:50:41 +0200

    fixed donation form

---
+++ components/global/SpendenFormular.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <iframe
+      src="https://secure.fundraisingbox.com/app/payment?hash=6j98n4k8fqegir0h#http%3A%2F%2Fwww.grenzensindrelativ.de%2Fspenden.html"
+      id="fbIframe"
+      title="Spendenformular"
+      style="width: 100%; height: 1648px"
+      scrolling="no"
+      allowtransparency="true"
+      width="100%"
+      height="1500"
+      frameborder="0"
+      ><h2>Spendenformular</h2>
+      <p>
+        Ihr Browser kann keine iFrames darstellen, bitte nutzen Sie folgenden
+        Link:
+        <a
+          href="https://secure.fundraisingbox.com/app/payment?hash=6j98n4k8fqegir0h#http%3A%2F%2Fwww.grenzensindrelativ.de%2Fspenden.html"
+          >Spendenformular</a
+        >
+      </p></iframe
+    >
+    <a target="_blank" href="https://www.fundraisingbox.com"
+      ><img
+        src="https://secure.fundraisingbox.com/images/FundraisingBox-Logo-Widget.png"
+        alt="FundraisingBox Logo"
+    /></a>
+  </div>
+</template>
