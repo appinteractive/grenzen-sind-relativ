0eab190d93d8f07f9094228c4292f00d561fa730
Grzegorz Leoniec <greg@app-interactive.de>
2020-10-05 14:32:52 +0200

    tighten postcard look

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <nuxt-link :to="link" class="bg-white group overflow-hidden rounded-sm border-gray-200 hover:shadow-2xl hover:border-gray-400 hover:z-0 transition duration-100">
+  <nuxt-link :to="link" class="post-card bg-white group overflow-hidden rounded-sm border-gray-200 hover:shadow-2xl hover:border-gray-400 hover:z-0 transition duration-100">
     <div v-if="image" class="relative pb-16/9">
       <img class="absolute w-full h-full object-cover" :src="image" alt="title">
     </div>
     <div style="max-height: 200px;" class="p-2 pb-8 overflow-hidden">
-      <h3 class="truncate font-medium tracking-wide group-hover:underline pb-2 text-gray-800" v-html="title" />
+      <h3 class="font-medium tracking-tight group-hover:underline pb-2 text-gray-800" v-html="title" />
       <p class="text-gray-700 font-normal" v-html="description" />
       <div class="absolute w-full bottom-0 left-0 from-white h-24 mb-4 bg-gradient-to-t">&nbsp;</div>
       <div class="absolute w-full bottom-0 left-0 h-4 bg-white">&nbsp;</div>
@@ -23,3 +23,12 @@ export default {
   }
 }
 </script>
+
+<style lang="postcss">
+.prose .post-card h3 {
+  @apply leading-tight;
+}
+.prose .post-card  {
+  @apply leading-tight;
+}
+</style>
